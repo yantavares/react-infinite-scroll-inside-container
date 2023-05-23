d4d12564c0304f3b778aee1a700ecbc4f42940ec
Yan Tavares <yantdo1@gmail.com>
2023-05-23 17:42:21 -0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="box">1</div>
+        <div className="box">2</div>
+        <div className="box">3</div>
+        <div className="box">4</div>
+        <div className="box">5</div>
+        <div className="box">6</div>
+        <div className="box">7</div>
+        <div className="box">8</div>
+        <div className="box">9</div>
+      </div>
     </div>
   );
 }
